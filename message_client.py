aba88df4b3ba5d42c9a126caabade52841e22bf7
logank <logan.aker1989@gmail.com>
2024-06-23 20:11:23 -0400

    adding a prompt to the message input

--- message_client.py
+++ message_client.py
@@ -49,7 +49,7 @@ t.daemon = True
 t.start()
 
 while True:
-    to_send = input()
+    to_send = input(f"\n >> ")
     if to_send.lower() == "q":
         break
     date_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
